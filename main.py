6ebec1e78b7c17200b7ccceb4d62491a0e29f3c4
ckdawg52 <ckdawg5250@gmail.com>
2026-03-25 20:55:59 +0000

    added tick to main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.time.Clock()
+    clock = pygame.time.Clock()
     dt = 0
     while True:
         log_state()
@@ -16,8 +16,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.time.clock.tick(60)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
