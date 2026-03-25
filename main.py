5a8ec8f04bb77bc713220209c47a274ec8ffd8bf
ckdawg52 <ckdawg5250@gmail.com>
2026-03-25 21:35:16 +0000

    fixed player

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.daraw(screen)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
